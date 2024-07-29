53699e0e30c8dcef5158ecdf3e603f9e3992ceaf
hyoni <jih70004@gmail.com>
2024-07-29 23:20:14 +0900

    리스트 삭제 기능 수정, 오타 수정, sortProducts 함수 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import style from './css/Product.module.css';
 import './css/App.css';
 
-function ProductList({ product, deletebtn }) {
+function ProductList({ product, deleteBtn }) {
   return (
     <>
       <li className={style.itemWrap}>
@@ -16,7 +16,7 @@ function ProductList({ product, deletebtn }) {
         <div className={style.item}>{product.description}</div>
         <div className={`${style.item} ${style.price}`}>가격: ₩{(product.price * 1200).toLocaleString()}</div>
         <div className={style.btnWrap}>
-          <button className={style.btn} onClick={deletebtn}>삭제</button>
+          <button className={style.btn} onClick={deleteBtn}>삭제</button>
         </div>
       </li>
     </>
@@ -29,22 +29,20 @@ function ProductContainer({ products }) {
   const [searchFilter, setSearchFilter] = useState('');
   const [sortList, setSortList] = useState('default');
 
-
-  const deletBtn = (e) => {
-    e.preventDefault();
-    const btnParent = e.target.closest('li')
-    let result = window.confirm(`정말 ${btnParent.querySelector('h3').textContent}을 삭제하시겠습니까?`);
+  const deleteBtn = (id) => {
+    // 삭제하려는 id를 먼저 찾는다.
+    // 해당 id를 제외한 나머지를 둔다.
+    let deleteItem = filterProducts.find(item => item.id === id);
+    // console.log(deleteItem.title)
+    let result = window.confirm(`정말 ${deleteItem.title}을 삭제하시겠습니까?`);
+    // console.log(result)
     if (result) {
       alert('성공적으로 삭제하였습니다.')
-      btnParent.remove();
+      setFilterProducts(filterProducts.filter(item => item.id !== id));
     }
   }
 
-  const getButtonStyle = (type) => {
-    return sortList === type ? { backgroundColor: '#87a86f', color: '#Fff' } : {};
-  };
-
-  useEffect(() => {
+  const sortProducts = (searchFilter, sortList, products) => {
     let filterList = products;
 
     if (searchFilter) {
@@ -63,6 +61,14 @@ function ProductContainer({ products }) {
     }
 
     setFilterProducts(filterList);
+  }
+
+  const getButtonStyle = (type) => {
+    return sortList === type ? { backgroundColor: '#87a86f', color: '#Fff' } : {};
+  };
+
+  useEffect(() => {
+    sortProducts(searchFilter, sortList, products);
   }, [searchFilter, sortList, products])
 
   return (
@@ -87,7 +93,7 @@ function ProductContainer({ products }) {
 
       <ul className="product-list">
         {filterProducts.map(product => (
-          <ProductList key={product.id} product={product} deletebtn={deletBtn} />
+          <ProductList key={product.id} product={product} deleteBtn={() => deleteBtn(product.id)} />
         ))}
       </ul>
     </div >
