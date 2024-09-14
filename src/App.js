7ad170baa4e02c5e18a8739be8f17ee0d5d426e7
hyoni <jih70004@gmail.com>
2024-09-14 16:34:24 +0900

    todoapp del

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ProductApp from "./pages/ProductApp";
 import Home from "./pages/Home";
 import Navigation from "./components/Navigation";
 import './resource/css/App.min.css';
-import ToDoApp from "./pages/ToDoApp";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ProductApp" element={<ProductApp />} />
-        <Route path="/todoApp" element={<ToDoApp />} />
       </Routes>
     </Router>
   )
