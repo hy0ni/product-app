beeb6a2064b3a8da99f2f25c091011576e7dfc55
hyoni <jih70004@gmail.com>
2024-09-30 03:18:37 +0900

    style 전체 수정, 코드 일부 수정

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import ProductApp from "./pages/ProductApp";
-import Home from "./pages/Home";
-import Navigation from "./components/Navigation";
+import ProductApp from "./pages/Products";
 import './resource/css/App.min.css';
 
 function App() {
   return (
     <Router basename="/product-app">
-      <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ProductApp" element={<ProductApp />} />
+        <Route path="/" element={<ProductApp />} />
       </Routes>
     </Router>
   )
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,29 +1,27 @@
 import { useEffect, useState } from "react";
-import '../resource/css/Product.min.css'
 
 function ProductList({ product, onDelete }) {
   return (
     <>
-      <li className='item-wrap'>
-        <div className='flex'>
+      <li className='product-list'>
+        <div className='list-visual'>
           <span className='item-rating'>평점: {product.rating.rate}</span>
           <figure className='item-img'>
             <img src={product.image} alt={product.title} />
           </figure>
         </div>
-        <h3 className='item'>{product.title}</h3>
-        <div className='item'>{product.description}</div>
-        <div className='item price'>가격: ₩{(product.price * 1200).toLocaleString()}</div>
-        <div className='btn-wrap'>
+        <h3 className='item item-title'>{product.title}</h3>
+        <div className='item item-description'>{product.description}</div>
+        <div className='item item-price'>가격: ₩{(product.price * 1200).toLocaleString()}</div>
+        <div className='list-btn-wrap'>
           <button className='btn' onClick={onDelete}>삭제</button>
         </div>
       </li>
     </>
-  )
+  );
 }
 
 function ProductContainer({ products }) {
-
   const [filterProducts, setFilterProducts] = useState(products);
   const [searchFilter, setSearchFilter] = useState('');
   const [sortList, setSortList] = useState('default');
@@ -34,13 +32,14 @@ function ProductContainer({ products }) {
     if (searchFilter) {
       const searchTerm = searchFilter.replace(/\s/g, '').toLowerCase();
       filterList = products.filter(product =>
-        product.title.replace(/\s/g, '').toLowerCase().includes(searchTerm));
+        product.title.replace(/\s/g, '').toLowerCase().includes(searchTerm)
+      );
     }
 
     if (sortList === 'price') {
       filterList.sort((a, b) => (a.price - b.price));
     } else if (sortList === 'rating') {
-      filterList.sort((a, b) => (b.rating.rate - a.rating.rate))
+      filterList.sort((a, b) => (b.rating.rate - a.rating.rate));
     }
 
     setFilterProducts(filterList);
@@ -48,24 +47,23 @@ function ProductContainer({ products }) {
 
   useEffect(() => {
     sortProducts(searchFilter, sortList, products);
-  }, [searchFilter, sortList, products])
-
+  }, [searchFilter, sortList, products]);
 
   const handleDelete = (title, id) => {
     if (id && window.confirm(`정말 ${title}을 삭제하시겠습니까?`)) {
-      alert('성공적으로 삭제하였습니다.')
+      alert('성공적으로 삭제하였습니다.');
       setFilterProducts(filterProducts.filter(item => item.id !== id));
     }
   }
 
   const getButtonStyle = (type) => {
-    return sortList === type ? { backgroundColor: '#87a86f', color: '#Fff' } : {};
+    return sortList === type ? { backgroundColor: '#87a86f', color: '#fff' } : {};
   };
 
   return (
     <div className='container'>
-      <div className='flex-wrap'>
-        <form>
+      <div className='search-wrap'>
+        <form className="search-form" onSubmit={(e) => e.preventDefault()}>
           <input
             type="text"
             placeholder="이름으로 검색"
@@ -78,8 +76,8 @@ function ProductContainer({ products }) {
         <div className='btnWrap'>
           <button onClick={() => setSortList('default')} style={getButtonStyle('default')} className='btn'>기본 정렬</button>
           <button onClick={() => setSortList('price')} style={getButtonStyle('price')} className='btn'>가격 정렬</button>
-          <button onClick={() => setSortList('rating')} style={getButtonStyle('rating')} className='btn'> 평점 정렬</button >
-        </div >
+          <button onClick={() => setSortList('rating')} style={getButtonStyle('rating')} className='btn'>평점 정렬</button>
+        </div>
       </div>
 
       <ul>
@@ -87,14 +85,15 @@ function ProductContainer({ products }) {
           <ProductList key={product.id} product={product} onDelete={() => handleDelete(product.title, product.id)} />
         ))}
       </ul>
-    </div >
-  )
+    </div>
+  );
 }
 
 function Products() {
   const [productData, setProductData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
       .then(response => {
@@ -111,7 +110,7 @@ function Products() {
         setError(error);
         setLoading(false);
       });
-  }, [])
+  }, []);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -121,6 +120,7 @@ function Products() {
       <h1 className='title'>상품 목록</h1>
       <ProductContainer products={productData} />
     </div>
-  )
+  );
 }
+
 export default Products;
